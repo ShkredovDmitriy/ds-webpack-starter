463e99fae94e6fadc1936fb81b87763975872a69
Dmitriy Shkredov <shkredovdmitriy@gmail.com>
2021-04-25 23:54:14 +0300

    Add ftp deploy

---
+++ config/deploy.js
@@ -0,0 +1,28 @@
+var FtpDeploy = require("ftp-deploy");
+var ftpDeploy = new FtpDeploy();
+require('dotenv').config();
+
+const user = process.env.FTP_DEPLOY_USER || "user";
+const password = process.env.FTP_DEPLOY_PASSWORD || "password";
+const host = process.env.FTP_DEPLOY_HOST || "host";
+const port = process.env.FTP_DEPLOY_PORT || "port";
+const remote_root = process.env.FTP_DEPLOY_REMOTE_ROOT || "/folder/subfolder/";
+
+var config = {
+    user: user,
+    password: password,
+    host: host,
+    port: port,
+    localRoot: __dirname + "/dist",
+    remoteRoot: remote_root,
+    include: ["*"],
+    exclude: ["*.map"],
+    deleteRemote: false,
+    forcePasv: false,
+    sftp: false
+};
+
+ftpDeploy
+     .deploy(config)
+     .then(res => console.log("finished:", res))
+     .catch(err => console.log(err));
--- webpack.config.js
+++ webpack.config.js
@@ -1,124 +1,127 @@
 /* eslint-disable no-dupe-keys */
 /* eslint-disable func-names */
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const fs = require('fs');
-const pug = require('./webpack/pug');
-const devserver = require('./webpack/devserver');
-const sass = require('./webpack/sass');
-const extractCSS = require('./webpack/css.extract');
-const css = require('./webpack/css');
-const sourceMap = require('./webpack/sourceMap');
-const lintJS = require('./webpack/js.lint');
-const lintCSS = require('./webpack/sass.lint');
-const images = require('./webpack/images');
-const filesLocations = require('./webpack/filesLocations');
-const fonts = require('./webpack/fonts');
-const babel = require('./webpack/babel');
-const favicon = require('./webpack/favicon');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const fs = require("fs");
+const pug = require("./config/pug");
+const devserver = require("./config/devserver");
+const sass = require("./config/sass");
+const extractCSS = require("./config/css.extract");
+const css = require("./config/css");
+const sourceMap = require("./config/sourceMap");
+const lintJS = require("./config/js.lint");
+const lintCSS = require("./config/sass.lint");
+const images = require("./config/images");
+const filesLocations = require("./config/filesLocations");
+const fonts = require("./config/fonts");
+const babel = require("./config/babel");
+const favicon = require("./config/favicon");
 
 const PATHS = {
-  source: path.join(__dirname, 'dev'),
-  build: path.join(__dirname, 'build'),
+  source: path.join(__dirname, "dev"),
+  build: path.join(__dirname, "build")
 };
 
 function generateHtmlPlugins(templateDir) {
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
   return templateFiles.map(item => {
-    const parts = item.split('.');
+    const parts = item.split(".");
     const name = parts[0];
-    const extension = 'pug';
+    const extension = "pug";
     const html = new HtmlWebpackPlugin({
       filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}/${name}.${extension}`),
-      inject: true,
+      template: path.resolve(
+        __dirname,
+        `${templateDir}/${name}/${name}.${extension}`
+      ),
+      inject: true
     });
     return html;
   });
 }
 
-const htmlPages = generateHtmlPlugins('./dev/pages');
+const htmlPages = generateHtmlPlugins("./dev/pages");
 
 const commonDev = merge([
   {
     entry: {
-      main: `${PATHS.source}/app/main.js`,
+      main: `${PATHS.source}/app/main.js`
     },
     output: {
       path: PATHS.build,
-      filename: './js/[name].js?[hash]',
+      filename: "./js/[name].js?[hash]"
     },
     plugins: [
       new webpack.ProvidePlugin({
-        Promise: 'es6-promise-promise',
+        Promise: "es6-promise-promise"
       }),
       new Dotenv(),
       new WriteFilePlugin(),
-      new CopyPlugin(filesLocations),
+      new CopyPlugin(filesLocations)
     ].concat(htmlPages),
     optimization: {
       minimizer: [
         new UglifyJsPlugin({
           cache: true,
           parallel: true,
-          sourceMap: false,
-        }),
-      ],
-    },
+          sourceMap: false
+        })
+      ]
+    }
   },
   pug(),
   images(),
   fonts(),
-  babel(),
+  babel()
 ]);
 
 const commonProd = merge([
   {
     entry: {
-      main: `${PATHS.source}/app/main.js`,
+      main: `${PATHS.source}/app/main.js`
     },
     output: {
       path: PATHS.build,
-      filename: './js/[name].js?[hash]',
+      filename: "./js/[name].js?[hash]"
     },
     plugins: [
       new webpack.ProvidePlugin({
-        Promise: 'es6-promise-promise',
+        Promise: "es6-promise-promise"
       }),
       new Dotenv(),
       new WriteFilePlugin(),
-      new CopyPlugin(filesLocations),
+      new CopyPlugin(filesLocations)
     ].concat(htmlPages),
     optimization: {
       minimizer: [
         new UglifyJsPlugin({
           cache: true,
           parallel: true,
-          sourceMap: true,
+          sourceMap: true
         }),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
-    },
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    }
   },
   pug(),
   images(),
   fonts(),
   babel(),
-  favicon(),
+  favicon()
 ]);
 
 module.exports = function(env, argv) {
-  if (argv.mode === 'production') {
+  if (argv.mode === "production") {
     return merge([commonProd, extractCSS(), favicon()]);
   }
-  if (argv.mode === 'development') {
+  if (argv.mode === "development") {
     return merge([commonDev, devserver(), sass(), css(), sourceMap()]);
   }
 };
