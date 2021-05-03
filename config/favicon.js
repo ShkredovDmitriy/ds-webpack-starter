2c0f74f2e2bede161b341ad7496482958ba5558d
Dmitriy Shkredov <shkredovdmitriy@gmail.com>
2021-05-04 00:54:25 +0300

    fix favicons

--- config/favicon.js
+++ config/favicon.js
@@ -4,31 +4,22 @@ module.exports = function() {
   return {
     plugins: [
       new FaviconsWebpackPlugin({
-        logo: './dev/favicon.png',
+        logo: './dev/assets/favicon/favicon.png',
+        cache: true,
+        publicPath: '/build',
         outputPath: '',
-        prefix: 'favicon.',
+        prefix: 'favicon/',
+        inject: true,
         favicons: {
-          icons: {
-            // Platform Options:
-            // - offset - offset in percentage
-            // - background:
-            //   * false - use default
-            //   * true - force use default, e.g. set background for Android icons
-            //   * color - set background for the specified icons
-            //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
-            //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
-            //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
-            //
-            android: true, // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            coast: false, // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            favicons: true, // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            firefox: false, // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            windows: false, // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-            yandex: false, // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-          },
-        },
+          android: false,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          appleIcon: false,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          appleStartup: false,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          coast: true,                // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          firefox: false,              // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          windows: false,             // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+          yandex: false                // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }` or an array of sources
+        }
       }),
     ],
   };
