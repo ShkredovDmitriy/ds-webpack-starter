59e520489cafdd63767035be29f44f5a36c6521b
dima <shkredovdmitriy@gmail.com>
2021-06-05 12:47:40 +0300

    refactoring

--- dev/app/_helpers/log.js
+++ dev/app/_helpers/log.js
@@ -1,8 +1,8 @@
 const log = (title, body) => {
   if (process.env.NODE_ENV === "development") {
-    console.log('//=======' + title + '======//');
+    console.log("/******** " + title + " ********/");
     console.log(body);
-    console.log('//=======================//');
+    console.log("/************************/");
   }
 };
 
