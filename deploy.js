e56921c82196ca7e50c05db5efda210eed6b10dd
Shkredov Dmitriy <shkredovdmitriy@gmail.com>
2021-06-20 16:44:36 +0300

    fix deploy bug

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
-require('dotenv').config();
+require("dotenv").config();
 
 const user = process.env.FTP_DEPLOY_USER || "user";
 const password = process.env.FTP_DEPLOY_PASSWORD || "password";
@@ -9,20 +9,20 @@ const port = process.env.FTP_DEPLOY_PORT || "port";
 const remote_root = process.env.FTP_DEPLOY_REMOTE_ROOT || "/folder/subfolder/";
 
 var config = {
-    user: user,
-    password: password,
-    host: host,
-    port: port,
-    localRoot: __dirname + "/dist",
-    remoteRoot: remote_root,
-    include: ["*"],
-    exclude: ["*.map"],
-    deleteRemote: false,
-    forcePasv: false,
-    sftp: false
+  user: user,
+  password: password,
+  host: host,
+  port: port,
+  localRoot: __dirname + "/build",
+  remoteRoot: remote_root,
+  include: ["*"],
+  exclude: ["*.map"],
+  deleteRemote: false,
+  forcePasv: false,
+  sftp: false
 };
 
 ftpDeploy
-     .deploy(config)
-     .then(res => console.log("finished:", res))
-     .catch(err => console.log(err));
+  .deploy(config)
+  .then(res => console.log("finished:", res))
+  .catch(err => console.log(err));
